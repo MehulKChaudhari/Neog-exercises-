293cdc2d70a7e179a434b120f1c77682f3869d9a
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2021-03-17 16:42:33 +0530

    added Async exercises

---
+++ FP/Aysnc/async.js
@@ -0,0 +1,67 @@
+//Question 6 HomeWork Async live coding Notion ## setInterval
+// - write a function which takes a message and time. The function should print that message every X interval.
+
+
+const messagePrinter = (message, delay) =>{
+    const printing = setInterval(() => console.log(message),delay)
+  }
+  
+  messagePrinter("Goooooddd Morninggggg!!!!!!", 100)
+
+
+
+//- Write a function which takes a number. Then print a countdown from that number to 0. At zero print "Bang Bang!"
+
+    const timer = (curr,fin) => {
+        let current = curr;
+        const countDown = setInterval(() =>{
+        console.log(current)
+        if (current==fin){
+        clearInterval(countDown)
+        console.log("Bang Bang!!")
+        }
+        current--
+        },1000);
+    }
+    
+    timer(10,0)
+
+    //Do all the promises exercises using Async/Await 
+
+    // ex:11 Print Data on Success 
+
+      function fakeFetch(msg, shouldReject) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (shouldReject) {
+              reject(`error from server: ${msg}`);
+            }
+            resolve(`from server: ${msg}`);
+          }, 3000);
+        });
+      }
+      
+      const printdata= async(msg) =>{
+        const data = await fakeFetch(msg);
+        console.log(data)
+      }
+      
+      printdata("Mehul");
+
+      // question ex12: print data on success, print error on failure
+
+        const printdata= async(msg,shouldReject) =>{
+          try{
+            const data = await fakeFetch(msg,shouldReject);
+            console.log(data)
+          }
+          catch(err){
+            console.log(err)
+        
+          }
+        }
+        printdata("Mehul"); 
+        //printdata("Mehul",true); //If want to check error handling uncomment this and comment the abouve function call
+
+
+
