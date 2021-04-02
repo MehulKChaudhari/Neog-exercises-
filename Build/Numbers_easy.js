c106f227a8c6193812e2fac3d307028d211400c4
Mehul Chaudhari <chaudharimehul2001@gmail.com>
2021-04-03 00:20:09 +0530

    Added Number Questions

---
+++ Build/Numbers_easy.js
@@ -0,0 +1,97 @@
+//Question 1 Write a program to input 2 numbers and display the sum of the numbers to the console.
+
+        // const sum = (a, b) =>{
+        //     console.log(`The sum of ${a} and ${b} is:`,a+b);
+        //   }
+        
+        //   sum(10,154)
+
+//question 2 Write a JavaScript program to calculate the simple interest given P,R,T with the given formula. Formula: SI = (P * T * R) / 100 Where: P = principal amount T = time R = rate SI = simple interest
+
+
+        // var readlineSync = require('readline-sync');
+
+        // var p = readlineSync.question('What is principal amount? ');
+
+        // var t = readlineSync.question('For how much period you are giving?');
+
+        // var r = readlineSync.question('what is rate of interest?(In %)');
+
+        // const simpleInterestCalculator =(p,t,r) => {
+        // SI = (p *  t * r) / 100;
+        // console.log("The simple interest is:", SI);
+        // }
+
+        // simpleInterestCalculator(p,t,r);
+
+//Question 3 Write a program to calculate the kinetic energy of a body with mass 'm' and volume 'v'. 
+
+    // var M = readlineSync.question('What is the mass of body? ');
+
+    // var V = readlineSync.question('what is volume of body '); 
+
+    // const kineticEnergyCalculator = (m,v) =>{
+    //   KE = 0.5 * m * v * v;
+    //   console.log(`The kinetic energy of body with mass ${m} and voloume ${v} is :`, KE);
+    // }
+
+    // kineticEnergyCalculator(M,V);
+
+//Question 5 Calculate the area, perimeter of a s1.re of side 'a'. Also, calculate the surface area and the volume of a cube of side 'a'.
+
+    // var side = readlineSync.question("Enter the side of Square and cube of which you want to find area and stuff:");
+
+    // const areaMeasurementCalculator = (a) => {
+    // Area= a*a
+
+    // Perimeter= 4*a
+
+    // Surface_Area= 6*a*a
+
+    // Volume = a*a*a; 
+
+    // console.log(`The area of Square with side ${a} is :`,Area);
+    // console.log(`The Perimeter of Square with side ${a} is :`, Perimeter);
+    // console.log(`The surface area of cube with side ${a} is :`, Surface_Area);
+    // console.log(`the Volume of cube with side ${a} is:`, Volume);
+    // }
+
+    // areaMeasurementCalculator(side);
+
+
+//Question 6 Given the Cost Price(CP) and Selling Price(SP) of a product. Write a Program to Calculate the Profit or Loss. 
+
+        // var CP = readlineSync.question('Enter the Cost Price of product:');
+        
+        // var SP = readlineSync.question('Enter the selling Price of product:');
+
+        // const profitOrLossCalculator = (cp, sp) =>{
+        //   output = sp - cp ;
+
+
+        //   if(output<0){
+        //     console.log(`The product with CP ${cp} and SP ${sp} produced you :`,Math.abs(output), "rupees loss" );
+        //   }
+        //   else if(output > 0 ){
+        //     console.log(`The product with CP ${cp} and SP ${sp} produced you :`,output,"Rupees Profit" );
+        //   }
+        //   else{
+        //     console.log(`The product with CP ${cp} and SP ${sp} produced you : No ProFit No Loss` );
+        //   }
+        // }
+
+        // profitOrLossCalculator(CP,SP);
+
+ // Question 7 Write a program to calculate sum of N natural digits, as input by the users?
+    // var sum=0;
+    // var n = readlineSync.question('Enter to count sum :');
+    // const sumOfnNumbers = (n) =>{
+    // for (i=0;i<=n;i++){
+    // sum=i+sum;
+    // }
+    // console.log("the sum is :", sum);
+    // }
+
+    // sumOfnNumbers(n);
+
+//Question 
